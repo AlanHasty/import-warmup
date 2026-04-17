97b77c815af63eb3ed3064be4e7956d5327833e1
Alan Hasty <github@hasty.info>
2026-04-17 16:32:07 +0000

    feat:Exercise 2 done

--- run_exercises.py
+++ run_exercises.py
@@ -1,3 +1,8 @@
+import portfolio.data
+from portfolio.data import create_portfolio
+from portfolio.report import print_report
+
+
 if __name__ == "__main__":
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
